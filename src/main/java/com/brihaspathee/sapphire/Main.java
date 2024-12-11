fef916baabdfb783c5304a45fae9d33281483746
vbalaji215-dev <vbalaji215@outlook.com>
2024-12-11 16:18:54 -0500

    Created API Response Constants

---
+++ src/main/java/com/brihaspathee/sapphire/Main.java
@@ -0,0 +1,16 @@
+package com.brihaspathee.sapphire;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 29, November 2024
+ * Time: 7:49 AM
+ * Project: Default (Template) Project
+ * Package Name: com.brihaspathee.sapphire
+ * To change this template use File | Settings | File and Code Template
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/brihaspathee/sapphire/constants/ApiResponseConstants.java
@@ -0,0 +1,18 @@
+package com.brihaspathee.sapphire.constants;
+
+/**
+ * Created in Intellij IDEA
+ * User: Balaji Varadharajan
+ * Date: 29, November 2024
+ * Time: 1:21 PM
+ * Project: sapphire
+ * Package Name: com.brihaspathee.sapphire.constants
+ * To change this template use File | Settings | File and Code Template
+ */
+public class ApiResponseConstants {
+
+    public static final String SUCCESS = "Success";
+    public static final String FAIL = "Fail";
+    public static final String API_SUCCESS = "100000";
+    public static final String API_FAIL = "110000";
+}
